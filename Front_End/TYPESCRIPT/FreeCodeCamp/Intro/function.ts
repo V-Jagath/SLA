0a3f750a586b4ea8f7923b36aecb3bac2a6fa837
V-Jagath <jagath9360@gmail.com>
2025-12-03 19:47:42 +0530

    Typescript Function updated

--- Front_End/TYPESCRIPT/FreeCodeCamp/Intro/function.ts
+++ Front_End/TYPESCRIPT/FreeCodeCamp/Intro/function.ts
@@ -1,8 +1,12 @@
+// 1. Number 
+
 function add(num: number) {
     return num + 2
 }
 
 add(23)
+
+// 2. String 
 
 function toUpperCase(val: string) {
     return val.toLocaleUpperCase()
@@ -10,14 +14,47 @@ function toUpperCase(val: string) {
 
 toUpperCase("jagath")
 
+// 3. Perameter Types
+
 function signIn(name: string, email: string, mobile: number, ispaid: boolean) {
     console.log(`Name:${name},email:${email},mobile:${mobile},isPaid:${ispaid}`)
 };
 
 signIn("jagath", "jagath9360@gmail.com", 24453, true);
 
-let login = (name: string, email: string, age: number, ispaid: boolean = false) => {
+// 4. Default Parameter
 
+let login = (name: string, email: string, age: number, ispaid: boolean = false) => {
+    console.log(`Name:${name},email:${email},age:${age},isPaid:${ispaid}`)
 }
 
 login("suriya", "suriya@gmail.com", 23) // not error because default parameter is here.
+
+
+// 5.(parameter: type): returnType => {}
+
+// const hero = ["thor", "spider Man", "ironMan"]
+const hero = [1, 2, 3]
+
+hero.map((e): string => {
+    return `Heros are ${e}` //2
+})
+
+
+// what do is the function always return a string values.
+
+// 6. Void - The function does not return any value.
+
+function consoleError(errMsg: string): void {
+    console.log(errMsg);
+    // return errMsg
+}
+
+// you can use void don't return anything
+
+// 7. Never - The function with never not return or always throw an error.
+
+function handleError(errMsg: string): never {
+    throw new Error(errMsg)
+}
+
---
+++ Front_End/TYPESCRIPT/FreeCodeCamp/Intro/object.ts
@@ -0,0 +1,11 @@
+// const user: { name: string; email: string; isActive: boolean } = {
+//     name: "Jagath",
+//     email: "jagath@9360",
+//     isActive: true
+// }
+
+function createUser({ name: string, isPaid: boolean }) {
+    return `${name} and it ${isPaid}`
+}
+
+createUser({ "jagath", true})
